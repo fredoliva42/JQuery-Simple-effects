f73b0d83f6e5ffc0fa16c353ff2df21e4b9bcd2c
Frederic Oliva <fredoliva42@gmail.com>
2017-03-05 01:38:43 -0800

    added button to change background color

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,34 @@ $(document).ready(function() {
     $(".initially-showing").slideToggle();
     $(".initially-hidden").slideToggle();
   });
-  
+
   $(".clickable2").click(function() {
     $(".secondary-showing").slideToggle();
     $(".secondary-hidden").slideToggle();
   });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  })
 });
