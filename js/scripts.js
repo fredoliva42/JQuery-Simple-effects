c8614c903ed760f218613d8d275b4b203b9a77f6
Frederic Oliva <fredoliva42@gmail.com>
2017-03-05 00:50:19 -0800

    added other pictures slide toggling and created new css classes, new JQuery code, for sliding the pictures separately + added anchor tag with link to Wiki Walrus page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").slideToggle();
-    $("#initially-hidden").slideToggle();
+    $(".initially-showing").slideToggle();
+    $(".initially-hidden").slideToggle();
+  });
+  $(".clickable2").click(function() {
+    $(".secondary-showing").slideToggle();
+    $(".secondary-hidden").slideToggle();
   });
 });
